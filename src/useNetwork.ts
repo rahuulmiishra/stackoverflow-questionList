e8e45baa709b33d7f7ae0d3b772491b9b5f75d18
Rahul Mishra <rahul.mishra@kickdrumtech.com>
2020-08-06 21:02:47 +0530

    Updated readme

--- src/useNetwork.ts
+++ src/useNetwork.ts
@@ -1,39 +1,10 @@
 import { useState } from "react";
 
+import { getFormattedDate, getTransformedQuestion } from "./utils/Utilities";
+
 import IQuestion from "./interfaces/IQuestion";
 
 const DEFAULT_PAGE_NUMBER = 1;
-
-const getFormattedDate = (timeStamp_value: number = 0): string => {
-  const date: Date = timeStamp_value
-    ? new Date(timeStamp_value * 1000)
-    : new Date();
-  return date.toUTCString();
-};
-
-const getTransformedQuestion = (questions = []): Array<IQuestion> => {
-  const questionList: Array<IQuestion> = [];
-  questions.forEach((question) => {
-    const {
-      title = "",
-      question_id = "",
-      link = "",
-      creation_date = 0,
-      owner,
-    } = question;
-    const { display_name: ownerName = "", link: ownerLink = "" } = owner;
-    const formattedQuestion: IQuestion = {
-      authorName: ownerName,
-      authorProfileLink: ownerLink,
-      creationDate: getFormattedDate(creation_date),
-      questionId: question_id,
-      questionLink: link,
-      title,
-    };
-    questionList.push(formattedQuestion);
-  });
-  return questionList;
-};
 
 const useNetwork = () => {
   const [isQuestionsLoading, updateQuestionsLoadingState] = useState(false);
